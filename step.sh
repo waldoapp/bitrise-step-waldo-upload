f4aef3aa7356aaafac70133271f2074d09e637e4
J. G. Pusey <eBardX@users.noreply.github.com>
2020-10-09 18:22:00 -0400

    Updated to use latest WaldoCLI.sh; updated summary and description (#2)

--- step.sh
+++ step.sh
@@ -7,7 +7,16 @@ SCRIPT_DIR=$(unset CDPATH && cd "${0%/*}" &>/dev/null && pwd)
 export WALDO_UPLOAD_TOKEN="$upload_token"
 export WALDO_VARIANT_NAME="$variant_name"
 
-waldo_symbols=""
+waldo_build_flavor=""
+waldo_build_suffix=${build_path##*.}
+
+case $waldo_build_suffix in
+    apk)     waldo_build_flavor="Android" ;;
+    app|ipa) waldo_build_flavor="iOS" ;;
+    *)       waldo_build_flavor="unknown" ;;
+esac
+
+export WALDO_USER_AGENT_OVERRIDE=Waldo BitriseStep/${waldo_build_flavor} v1.0.0"
 
 if [[ -z $symbols_path && $find_symbols == true ]]; then
     exec "$SCRIPT_DIR"/WaldoCLI.sh "$build_path" --include_symbols
